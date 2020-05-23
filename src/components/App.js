5c74d6276e92f4b867df15103724ab4d98ee7e91
Arna <arnamaity6230@gmail.com>
2020-05-24 00:39:06 +0530

    Improved directory structure.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,49 @@
-import React, {Component} from 'react';
+// App Parent Component
+import React, { Component, Fragment } from 'react';
+import { Container, Typography, Paper } from '@material-ui/core';
 
-class App extends Component{
-    render(){
-        return(
-        <div>
-            <h1>My React App</h1>
-        </div>);
-    }
+// Bottom Navigation Bar Component
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import {
+  RestoreIcon,
+  FavoriteIcon,
+  LocationOnIcon,
+  FolderIcon,
+} from '@material-ui/icons';
+
+// Appbar Component
+import { AppBar, Toolbar } from '@material-ui/core';
+
+// Importing Child Components
+import { Header, Footer } from './layouts';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {}
+
+  render() {
+    return (
+      <Fragment>
+        <Paper elevation={3} variant="outlined">
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="headline" color="inherit">
+                HavocOS
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Container maxWidth="lg">
+            <Header />
+            <Footer />
+          </Container>
+        </Paper>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,25 @@ const htmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, '/build'),
+        publicPath: '/',
         filename: "index_bundle.js"
     },
+    devServer: {
+        contentBase: './build',
+      },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use:{
-                    loader: 'babel-loader'
-                }
+                use: ['babel-loader']
             }
         ]
     },
     plugins: [
         new htmlWebpackPlugin({
-            template: "./src/index.html"
+            template: path.resolve('./index.html')
         })
     ]
-}
+};
